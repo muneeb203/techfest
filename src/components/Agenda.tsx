fc8f8465c6b2c739d12ef0efad3265bf24a0ce60
Muneeb ur Rehman Qureshi <149325761+muneeb203@users.noreply.github.com>
2025-10-18 13:06:50 +0500

    Fix sponsorship package download link path

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -71,7 +71,7 @@ const Agenda = () => {
 
   const handleSponsorshipPackageDownload = () => {
     const link = document.createElement('a');
-    link.href = 'public\\Techfest Sponsorship Proposal.pdf';
+    link.href = 'Techfest Sponsorship Proposal.pdf';
     link.download = 'Techfest Sponsorship Proposal.pdf';
     document.body.appendChild(link);
     link.click();
